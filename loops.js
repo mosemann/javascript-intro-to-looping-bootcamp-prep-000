883286c6c3c0ac7ff39709851b3b92f11755352e
Joel Mosemann <mosemann@hotmail.com>
2017-12-06 21:29:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,5 @@
 function forLoops() {
   for (let i = 0; i < 25; i++)
-  if (i = 0) return "I'm zero!"
   if (i=1) return "I am 1 strange loop."
-  if (i)
+  else return "I am ${i} strange loops."
 }
