8935a40ffe5a3e42ddef7a70985e06ceaee3f114
Joel Mosemann <mosemann@hotmail.com>
2017-12-06 22:12:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,4 +4,9 @@ function forLoop(a) {
   else {a.push("I am ${i} strange loops.")}
 }return a
 }
-
+function whileLoop(n) {
+  let countdown = n
+  while (countdown > 0) {
+    console.log(--countdown)
+  }return "done"
+}
